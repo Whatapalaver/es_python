a3d78c71d315aead62e32051efd168dc34d81576
whatapalaver <warriorwomenblog@gmail.com>
2019-03-13 17:21:19 +0000

    Adding the _explain output to terminal output

--- app/routes/search.py
+++ app/routes/search.py
@@ -9,6 +9,31 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9100}])
 search_blueprint = Blueprint('search',__name__,template_folder="templates")
 search_term = "building"
 
+# helper functions
+
+# Some utilities for flattening the explain into something a bit more
+# readable. Pass Explain JSON, get something readable (ironically this is what Solr's default output is :-p)
+def flatten(l):
+    [item for sublist in l for item in sublist]
+
+def simplerExplain(explainJson, depth=0):
+    result = " " * (depth * 2) + "%s, %s\n" % (explainJson['value'], explainJson['description'])
+    #print json.dumps(explainJson, indent=True)
+    if 'details' in explainJson:
+        for detail in explainJson['details']:
+            result += simplerExplain(detail, depth=depth+2)
+    return result
+    
+def analyze(text, field=None, analyzer=None):
+    whatToAnalyze = ''
+    if field is not None:
+        whatToAnalyze = "field=%s" % field
+    elif analyzer is not None:
+        whatToAnalyze = "analyzer=%s" % analyzer
+    resp = requests.get("http://localhost:9100/objects_010319/_analyze?%s&format=yaml" % whatToAnalyze, 
+                        data=text)
+    print(resp.text)
+
 
 @search_blueprint.route('/', methods=['GET', 'POST'], endpoint='index')
 def index():
@@ -26,10 +51,10 @@ def index():
       print("Search Term:", search_term)
       payload = {
         "query": {
-          "query_string": {
-              "analyze_wildcard": True,
+          "multi_match": {
               "query": str(search_term),
-              "fields": ["physicalDescription", "object"]
+              "fields": ["physicalDescription", "object", "metadata^3"],
+              "type": "most_fields"
           }
         },
         "size": 50,
@@ -37,6 +62,14 @@ def index():
 
         ]
       }
-      res = es.search(index="objects_010319", body=payload)
+      res = es.search(index="objects_010319", body=payload, explain=True)
+
+      # print(json.dumps(res['hits']['hits'][0]['_explanation'], indent=2))
+
+      print('*******Simple explain*********')
+      print('')
+      for hit in res['hits']['hits']:
+        print(simplerExplain(hit['_explanation']))
+      
       return render_template('index.html', res=res)
-      # return jsonify(res['hits']['hits'])
+
